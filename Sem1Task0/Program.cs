1a856a7724ac69b5c80138910abd8ab990b60bbc
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-04 20:54:08 +0300

    task0 done

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №0
+//Напишите программу, которая на входе принимает число и выдает его квадрат (число умноженное на само себе)
+Console.WriteLine("Введите число: ");
+//Считываем данные с консоли
+string? inputNum = Console.ReadLine();//??"0";
+//Проверяем, чтобы данные не были пустыми
+if (inputNum != null)
+{
+    //Парсим введеное число
+    //int num = int.Parse(inputNum);
+    //Находим квадрат числа
+    //int res = num * num;
+    //Выводим данные в консоль
+    //Console.WriteLine(res);
+Console.WriteLine("Квадрат числа: " + (int)Math.Pow(int.Parse(inputNum), 2));
+}
