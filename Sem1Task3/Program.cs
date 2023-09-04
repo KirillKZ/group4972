56eb93021599db57c823b7889aaa5bc7db9085c7
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-04 21:35:42 +0300

    task3 add alt

--- Sem1Task3/Program.cs
+++ Sem1Task3/Program.cs
@@ -1,5 +1,5 @@
-﻿System.Console.Line("Введите день недели:");
-int day = int.Parse(Console.ReadLine()??"0");
+﻿System.Console.WriteLine("Введите день недели:");
+int day = int.Parse(Console.ReadLine() ?? "0");
 string[] dayOfWeek = new string[7];
 dayOfWeek[0] = "Понедельник";
 dayOfWeek[1] = "Вторник";
@@ -8,4 +8,12 @@ dayOfWeek[3] = "Четверг";
 dayOfWeek[4] = "Пятница";
 dayOfWeek[5] = "Суббота";
 dayOfWeek[6] = "Воскресенье";
-System.Console.WriteLine(dayOfWeek[day - 1]);
+if (day >= 0 && day <= 6){
+    System.Console.WriteLine(dayOfWeek[day - 1]);
+}
+else
+{
+    System.Console.WriteLine("это не день недели");
+}
+
+//string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum));
