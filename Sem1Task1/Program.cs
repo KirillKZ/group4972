ae1a0e12a6eb4f69ba2a870467ba22fb59a9325c
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-04 21:11:08 +0300

    task1 done

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите первое число:");
+int numA = int.Parse(Console.ReadLine()?? "0") ;
+System.Console.WriteLine("Введите второе число:");
+int numB = int.Parse(Console.ReadLine()?? "0") ;
+if (numB * numB == numA)
+{
+    System.Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    System.Console.WriteLine("Первое число не является квадратом второго");
+}
