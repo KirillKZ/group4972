420d62d9c89caeb5a91f56d4a1975942dae70b0e
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-04 21:46:24 +0300

    task5 done

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача №5
+//Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+System.Console.WriteLine("Введите число:");
+int numN = int.Parse(Console.ReadLine() ?? "0");
+for (int i = numN * -1; i <= numN; i++)
+{
+    Console.Write(i + " ");
+}
