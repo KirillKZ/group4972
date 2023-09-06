62f05561778bb165e933a5fe7f55eb2e7b118297
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-06 22:14:00 +0300

    Task2 done

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -2,9 +2,9 @@
 //Напишите программу, которая на вход принимает два числа и проверяет, 
 //является ли первое число квадратом второго.
 System.Console.WriteLine("Введите первое число:");
-int numA = int.Parse(Console.ReadLine()?? "0") ;
+int numA = int.Parse(Console.ReadLine() ?? "0") ;
 System.Console.WriteLine("Введите второе число:");
-int numB = int.Parse(Console.ReadLine()?? "0") ;
+int numB = int.Parse(Console.ReadLine() ?? "0") ;
 if (numB * numB == numA)
 {
     System.Console.WriteLine("Первое число является квадратом второго");
---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+System.Console.WriteLine("Введите первое число: ");
+int first = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите второе число: ");
+int second = int.Parse(Console.ReadLine() ?? "0");
+int max = first > second ? first : second;
+Console.WriteLine("max = " + max);
