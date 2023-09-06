b1e37a9bc92102289bf9a3b564b86721f1c45bcf
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-06 22:28:08 +0300

    task4 done

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+int[] array = new int[3];
+System.Console.WriteLine("Введите первое число: ");
+array[0] = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите второе число: ");
+array[1] = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите третье число: ");
+array[2] = int.Parse(Console.ReadLine() ?? "0");
+int max = array[0];
+for (int i = 1; i < array.Length; i++) if (array[i] > max) max = array[i];
+System.Console.WriteLine(max);
