059a4ecd7ed2736a771c76d193660405ac9db929
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-06 22:53:28 +0300

    task8 done

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+string str = "";
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        str = str + i + ", ";
+    }
+}
+Console.Write(str.TrimEnd(new Char[] { ' ', ',' }));
