c0215e815568643110cb6671feb222a28ceca5e0
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-07 21:35:50 +0300

    task12 done

---
+++ Sem1Task12/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №12
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. 
+// Если второе число некратно первому, то программа выводит остаток от деления.
+System.Console.WriteLine("Введите первое число: ");
+int first = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите второе число: ");
+int second = int.Parse(Console.ReadLine() ?? "0");
+String result = (second%first == 0) ? "Второе кратно первому" : "Второе не кратно первому" + ". Остаток от деления: " + second%first;
+System.Console.WriteLine(result);
