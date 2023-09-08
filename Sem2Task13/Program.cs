eb0c8c0a24ae1581613902484f4b780e9130f5a7
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-08 21:29:20 +0300

    task13 done

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿//№13 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+//* Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+
+Console.WriteLine("Enter a number of less than 10 digits: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int length_num = 0;
+int[] digits = new int[10];
+while (num > 0)
+{
+    digits[length_num] = num % 10;
+    num = num / 10;
+    length_num++;
+}
+
+if (length_num < 3)
+{
+    Console.WriteLine("There is no third digit in this number");
+}
+else
+{
+    Console.WriteLine(digits[length_num - 3]);
+}
+
