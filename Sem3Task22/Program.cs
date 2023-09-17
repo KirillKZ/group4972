0ddbe8ec24b5ace317de7290aae83b06bb6e5f99
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-17 21:52:58 +0300

    task22 done

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №22
+// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// Например:
+// 5 -> 1, 4, 9, 16, 25. 
+// 2 -> 1,4
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+string BuildLine(int num, int pow)
+{
+    string strResult = String.Empty;
+    for (int i = 1; i <= num; i++)
+    {
+        strResult = strResult + Math.Pow(i, pow) + " ";
+    }
+    return strResult;
+}
+
+void PrintResult(string str)
+{
+    Console.WriteLine(str);
+}
+int n = ReadData("Введите число: ");
+PrintResult(BuildLine(n, 1));
+PrintResult(BuildLine(n, 2));
