e3ec02e1d42ee90934118a1d9a7bdb30e0e34300
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-20 21:14:57 +0300

    task29 done

---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// №29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// (задание со *) Ввести с клавиатуры длину массива и диапазон значений элементов
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+void PrintResult(int[] a)
+{
+    Console.WriteLine($"[{String.Join(",", a)}]");
+}
+
+
+int[] array(int arrLength, int fromRange, int toRange)
+{
+    int[] arr = new int[arrLength];
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(fromRange, toRange + 1);
+    }
+    return arr;
+
+}
+
+int arrLength = ReadData("Введите длину массива:");
+int fromRange = ReadData("Задайте начальную цифру диапазона значений:");
+int toRange = ReadData("Задайте конечную цифру диапазона значений:");
+int[] a = array(arrLength, fromRange, toRange);
+PrintResult(a);
