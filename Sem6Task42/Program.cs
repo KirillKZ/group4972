eb46355acfa6b229c6308b2e207c05818ddaf4c8
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-25 20:57:24 +0300

    task42 done

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №42 
+// Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное.
+// Например:
+// 45 -> 101101 
+// 3 -> 11 
+// 2 -> 10
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+void PrintResult(string res)
+{
+    Console.WriteLine(res);
+}
+
+string DecToBin(int num)
+{
+    string res = "";
+
+    while (num > 0)
+    {
+        res = (num % 2) + res;
+        num = num / 2;
+    }
+
+    return res;
+}
+
+int num = ReadData("Введите число: ");
+PrintResult(DecToBin(num));
+
