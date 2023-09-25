3d118230a9dbe85e09fb8e6806a4a1dae19fe51f
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-25 22:11:54 +0300

    task41 not done

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл 
+// пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+void PrintResult(string res)
+{
+    Console.WriteLine(res);
+}
+
+int BiggerZero (int num){
+
+}
