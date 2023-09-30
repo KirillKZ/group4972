d3ca5790e8c1265f29c76e0e405bd947a29c1dc5
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-09-30 23:48:59 +0300

    52star optimazed

--- Sem7Task52star/Program.cs
+++ Sem7Task52star/Program.cs
@@ -5,8 +5,9 @@
 // 5 9 2 3 
 // 8 4 2 4 
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-// * Дополнительно вывести среднее арифметическое по диагоналям и диагональ выделить разным 
-// цветом
+// * Дополнительно вывести среднее арифметическое по диагоналям и диагональ выделить разным цветом
+
+
 //создаем и заполняем двумерный массив
 int[,] CreateIncreasingMatrix(int n, int m, int k)
 {
@@ -26,93 +27,22 @@ int[,] CreateIncreasingMatrix(int n, int m, int k)
     return arr2D;
 }
 //находим значения диагоналей, раскрашиваем и выводим на экран, одновременно с этим создаем двумерный массив со значениями диагоналей
-int[,] CountAndPrintDiagonalSums(int[,] matrix)
+int[,] PrintColorDiagonals(int[,] matrix)
 {
-    ConsoleColor[] colors = new ConsoleColor[]
-        {ConsoleColor.White,
-         ConsoleColor.Blue,
-         ConsoleColor.Cyan,
-         ConsoleColor.DarkBlue,
-         ConsoleColor.DarkMagenta,
-         ConsoleColor.DarkRed,
-         ConsoleColor.DarkYellow,
-         ConsoleColor.Gray,
-         ConsoleColor.Green,
-         ConsoleColor.Red};
     int[,] diagNums = new int[matrix.GetLength(0) + (matrix.GetLength(1) - 1), matrix.GetLength(1)];
 
     for (int x = 0; x < matrix.GetLength(0); x++)
     {
         for (int y = 0; y < matrix.GetLength(1); y++)
         {
+            if (x - y >= 0) diagNums[x - y, y] = matrix[x, y];
+            else diagNums[diagNums.GetLength(0) + (x - y), y] = matrix[x, y];
 
-            if (x - y == 0)
-            {
-                Console.ForegroundColor = colors[1];
-                diagNums[x - y, y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == 1)
-            {
-                Console.ForegroundColor = colors[2];
-                diagNums[x - y, y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == 2)
-            {
-                Console.ForegroundColor = colors[3];
-                diagNums[x - y, y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == 3)
-            {
-                Console.ForegroundColor = colors[8];
-                diagNums[x - y, y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == 4)
-            {
-                Console.ForegroundColor = colors[9];
-                diagNums[x - y, y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == -1)
-            {
-                Console.ForegroundColor = colors[4];
-                diagNums[diagNums.GetLength(0) + (x - y), y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == -2)
-            {
-                Console.ForegroundColor = colors[5];
-                diagNums[diagNums.GetLength(0) + (x - y), y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else if (x - y == -3)
-            {
-                Console.ForegroundColor = colors[6];
-                diagNums[diagNums.GetLength(0) + (x - y), y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            else
-            {   Console.ForegroundColor = colors[0];
-                diagNums[diagNums.GetLength(0) + (x - y), y] = matrix[x, y];
-                System.Console.Write(matrix[x, y] + "\t");
-                Console.ResetColor();
-            }
-            
+            SetColor(matrix, x, y);
         }
         System.Console.WriteLine();
     }
-    
+
     return diagNums;
 }
 //находим среднюю арифметическую по диагоналям
@@ -140,13 +70,32 @@ void FindDiagonalAvrgAndPrinResult(int[,] matrix)
     }
 }
 
-//определяем размерность массива и шаг для заполнения значениями
+void SetColor(int[,] matrix, int x, int y)
+{
+    ConsoleColor[] colors = new ConsoleColor[]
+       {ConsoleColor.White,
+         ConsoleColor.Blue,
+         ConsoleColor.Cyan,
+         ConsoleColor.DarkBlue,
+         ConsoleColor.DarkMagenta,
+         ConsoleColor.DarkRed,
+         ConsoleColor.DarkYellow,
+         ConsoleColor.Gray,
+         ConsoleColor.Green,
+         ConsoleColor.Red};
+    Console.ForegroundColor = colors[5 + (x - y)];
+    System.Console.Write(matrix[x, y] + "\t");
+    Console.ResetColor();
+
+}
+
+//определяем размерность массива и шаг для заполнения значениями (размерность массива огрничена количеством цветов в палитре)
 int n = 5;
 int m = 5;
 int k = 1;
 
 int[,] matrix = CreateIncreasingMatrix(n, m, k);
 
-FindDiagonalAvrgAndPrinResult(CountAndPrintDiagonalSums(matrix));
+FindDiagonalAvrgAndPrinResult(PrintColorDiagonals(matrix));
 
 
