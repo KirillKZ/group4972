9e6e13bb224531c7745f8e2eefe6645a98702891
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-10-01 18:51:35 +0300

    task53 done

---
+++ Sem8Task53/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача №53 
+// Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю 
+// строку массива
+
+
+int[,] Create2DArray(int rows, int columns, int downLimit, int upLimit)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(downLimit, upLimit + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray2D(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+            
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SwapRowsInMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = temp;
+    }
+}
+
+int[,] array2D = Create2DArray(10, 10, 10, 99);
+PrintArray2D(array2D);
+System.Console.WriteLine();
+SwapRowsInMatrix(array2D);
+PrintArray2D(array2D);
