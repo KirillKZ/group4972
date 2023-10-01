5b5495f19ca8bfe658fb3588afb6f9ae9643222b
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-10-01 22:40:44 +0300

    Task61 done

---
+++ Sem8Task61/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №61
+// Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного 
+// треугольника
+
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintPaskalTriangle (int n)
+{
+    for (int y = 0; y < n; y++)
+            {
+                int c = 1;
+                for (int q = 0; q < n - y; q++)
+                {
+                    System.Console.Write("   ");
+                }
+ 
+                for (int x = 0; x <= y; x++)
+                {
+                    System.Console.Write($"   {c} ");
+                    c = c * (y - x) / (x + 1);
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+}
+
+int length = ReadData("Введите количество первых строк треугольника Паскаля:");
+
+PrintPaskalTriangle (length);
