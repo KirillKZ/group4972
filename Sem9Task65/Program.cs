bf55f58851f52dc99f7d9e9fe90f4a4d276c2fd9
kuzminkz <kuzmin.kir@gmail.com>
2023-10-04 22:56:36 +0300

    task65 done

---
+++ Sem9Task65/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 65.
+// Задайте значения М и N.
+// Напишите программу, которая найдет все натуральные элементы в промежутке от M до N.
+
+
+
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+string RecMN(int m, int n)
+{
+    string res = String.Empty;
+    
+    if (m >= n)
+    {
+        return res + m;
+    }
+    else
+    {
+      return res+=  m + " " + RecMN(m + 1, n) ;
+    }
+
+
+
+}
+
+int m = ReadData("Введите число M ");
+int n = ReadData("Введите число N ");
+System.Console.WriteLine(RecMN(m, n));
