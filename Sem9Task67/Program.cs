3bc89a5ae39b27ac6bee371710a05cb7d608c628
KirillKZ <143133626+KirillKZ@users.noreply.github.com>
2023-10-04 23:29:47 +0300

    Task67 done

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67
+// Программа принимает на вход число N и возвращает сумму цифр в числе
+
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int summOfNumber(int N)
+{
+    if (N == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return N % 10 + summOfNumber(N / 10);
+    }
+}
+
+
+System.Console.WriteLine(summOfNumber(ReadData("Введите число: ")));
+
