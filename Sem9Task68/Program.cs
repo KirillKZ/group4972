6d0e1147af7883df8c8065f5605ebcddf801db9c
kuzminkz <kuzmin.kir@gmail.com>
2023-10-05 16:41:56 +0300

    гз68

--- Sem9Task68/Program.cs
+++ Sem9Task68/Program.cs
@@ -11,8 +11,7 @@ long RecAkkerman(long m, long n)
 {
     if (m == 0)
         return n + 1;
-    else
-      if ((m != 0) && (n == 0))
+    else if ((m != 0) && (n == 0))
         return RecAkkerman(m - 1, 1);
     else
         return RecAkkerman(m - 1, RecAkkerman(m, n - 1));
